9bd1798b831fabb045c53dba034c64a5f72c057b
Jakoubekkk <91307618+Jakoubekkk@users.noreply.github.com>
2022-10-24 19:34:30 +0200

    yes

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,22 @@ fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+	window.onload = function () {
+		window.addEventListener('scroll', function (e) {
+			if (window.pageYOffset > 100) {
+				document.querySelector("header").classList.add('is-scrolling');
+			} else {
+				document.querySelector("header").classList.remove('is-scrolling');
+			}
+		});
+	
+		const menu_btn = document.querySelector('.nav-toggler');
+		const mobile_menu = document.querySelector('.nav');
+	
+		menu_btn.addEventListener('click', function () {
+			
+			menu_btn.classList.toggle('is-active');
+			mobile_menu.classList.toggle('open');
+		});
+	}
